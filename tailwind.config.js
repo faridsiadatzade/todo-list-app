f94b126ebe167f3a633a7c47cf2d0d31f3152a93
fsiadatzade <fsiadatzade@gmail.com>
2024-12-30 14:24:52 +0330

    [ADD]: initial config (root)

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+        40: "40px",
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "var(--primary)",
+          foreground: "var(--primary-foreground)",
+        },
+        secondary: {
+          DEFAULT: "",
+          foreground: "var(--secondary-foreground)",
+        },
+        border: "var(--primary)",
+        input: "var(--input)",
+        ring: "var(--ring)",
+        blue: "var(--blue)",
+        gray: {
+          DEFAULT: "var(--gray)",
+          light: "var(--light-gray)",
+          dark: "var(--dark-gray)",
+        },
+        red: {
+          DEFAULT: "var(--red)",
+        },
+        green: "var(--green)",
+        success: "var(--success)",
+      },
+      backgroundImage: {
+        "home-bg": "url('./src/assets/images/home-bg.jpg')",
+      },
+    },
+  },
+};
